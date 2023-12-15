7289580b7afda6cc9a6543a8ce850b35cfd524f3
Frans Budi Kashira <fransbudikashira@gmail.com>
2023-12-15 18:15:42 +0700

    commit ke-2

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended("/profile");
+            return redirect()->route("profile");
         }
 
         return back()->with(
